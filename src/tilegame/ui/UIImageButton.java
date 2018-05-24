2e969a053876543c62ebed6be4e84d9a0aef088f
Energy <Energy@Energy-PC>
2018-05-24 18:12:38 -0500

    UIImageButton now works properly

--- src/tilegame/ui/UIImageButton.java
+++ src/tilegame/ui/UIImageButton.java
@@ -14,7 +14,7 @@ public class UIImageButton extends UIObject{
 	
 	private BufferedImage[] images;
 	private Handler handler;
-	private boolean step1, activated;
+	private boolean step1, step2, activated;
 	/**
 	 * This constructor takes in the handler, location, width and height and the image texture array of a button to be added.
 	 * @param handler
@@ -37,18 +37,21 @@ public class UIImageButton extends UIObject{
 	public void update() {
 		if(bounds.contains(handler.getMouse().getX(), handler.getMouse().getY())) 
 			hovering = true;
-		
 		else 
 			hovering = false;
-		
-		//This part of the class could use some help because if player releases mouse button on any button, it activates.
-		if(hovering && handler.getMouse().isButtonDown(Mouse.LEFT_MOUSE))
+		//Clicking button without comming off of the button will activate it once		
+		if(!step1 && hovering && !handler.getMouse().isButtonDown(Mouse.LEFT_MOUSE)) {
 			step1 = true;
-		if(step1 && hovering && !handler.getMouse().isButtonDown(Mouse.LEFT_MOUSE))
+		} else if(step1 && hovering && handler.getMouse().isButtonDown(Mouse.LEFT_MOUSE)) {
+			step2 = true;
+		} else if(step1 && step2 && hovering && !handler.getMouse().isButtonDown(Mouse.LEFT_MOUSE)) {
 			activated = true;
-		
-		else
+		} else if (!hovering) {
+			step1 = false;
+			step2 = false;
+		} else {
 			activated = false;
+		}
 	}
 	/**
 	 * This method renders the updated button.
